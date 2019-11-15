3b45911fcf6fc65f699a2ad6181942eb3de3f6af
Frank Fang <frankfang2014@hotmail.com>
2019-11-15 12:57:20 -0800

    add firebase package

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
+import CustomButton from "../custom-button/custom-buttom.component";
 class SignIn extends Component {
   constructor(props) {
     super(props);
@@ -45,7 +46,9 @@ class SignIn extends Component {
             label="password"
           />
 
-          <input type="submit" value="Submit Form" />
+          <CustomButton type="submit" value="Submit Form">
+            SignIn
+          </CustomButton>
         </form>
       </div>
     );

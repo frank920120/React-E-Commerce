45a6744035791819b7a585225db504e95b8d7347
Frank Fang <frankfang2014@hotmail.com>
2019-11-14 21:58:32 -0800

    add custom input

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./sign-in.styles.scss";
-
+import FormInput from "../form-input/form-input.component";
 class SignIn extends Component {
   constructor(props) {
     super(props);
@@ -27,22 +27,24 @@ class SignIn extends Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <input
+          <FormInput
             type="email"
             name="email"
             value={email}
             required
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="email"
           />
-          <label htmlFor="email">Email</label>
-          <input
+
+          <FormInput
             type="password"
             name="password"
             value={password}
             required
-            onChange={this.handleChange}
+            handleChange={this.handleChange}
+            label="password"
           />
-          <label htmlFor="password">password</label>
+
           <input type="submit" value="Submit Form" />
         </form>
       </div>

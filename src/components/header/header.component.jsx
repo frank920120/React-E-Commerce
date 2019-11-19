67bc6733f82310f78250407bc92ccc64471bec76
Frank Fang <frankfang2014@hotmail.com>
2019-11-18 18:22:43 -0800

    add dropdown menu component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,11 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
-const Header = ({ currentUser }) => {
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
+
+const Header = ({ currentUser, isCartToggle }) => {
+  console.log(isCartToggle);
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -26,11 +30,15 @@ const Header = ({ currentUser }) => {
             Sign In
           </Link>
         )}
+        <CartIcon />
       </div>
+      {isCartToggle ? null : <CartDropDown />}
     </div>
   );
 };
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser: currentUser,
+  isCartToggle: hidden
 });
 export default connect(mapStateToProps)(Header);
